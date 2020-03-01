3318719671942eb98b49906d0aeb487edb0c8611
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-01 17:12:28 +0100

    Initial commit - very basic functions

---
+++ main.py
@@ -0,0 +1,21 @@
+#Python is a programming language
+
+#It is different from some other programming languages because it does
+#not use semi-colons to end a line.
+#There are also no curly brackets
+
+print("Hello, world!") #This single lline can run. It does not need to be placed in a main method
+
+
+#Indentation in Python is important as it defines scopes/blocks of code
+
+if 1 > 0:
+    print("1 is more than none")
+
+#Variable types are not declared
+
+var1 = "A random string"
+var2 = "100"
+
+print(var1)
+print(var2)
