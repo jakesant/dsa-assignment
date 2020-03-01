3d232a76efa214ece440d9da228c4001b9710614
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-01 22:58:45 +0100

    Added more varied data types

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #Python is a programming language
 
-#It is different from some other programming languages because it does
-#not use semi-colons to end a line.
-#There are also no curly brackets
+#It is different from some other programming languages because it does not use semi-colons to end a line.
+#There are also no curly brackets for scopes. Instead a : is used
 
 print("Hello, world!") #This single lline can run. It does not need to be placed in a main method
 
@@ -15,7 +14,17 @@ if 1 > 0:
 #Variable types are not declared
 
 var1 = "A random string"
-var2 = "100"
+var2 = "which is just boring"
 
 print(var1)
 print(var2)
+
+x, y, z = 1, 2, 3
+
+print("This is print statement number " + x)
+
+#Strings can be concatenated simply using a +
+
+var3 = var1 + var2
+
+print(var3)
---
+++ vars.py
@@ -0,0 +1,19 @@
+#Below are the following data types present in Python
+
+#Text Type:	str
+#Numeric Types:	int, float, complex
+#Sequence Types:	list, tuple, range
+#Mapping Type:	dict
+#Set Types:	set, frozenset
+#Boolean Type:	bool
+#Binary Types:	bytes, bytearray, memoryview
+
+x = str("Data") #Here the data type has been manually set by us
+
+num1 = 1 #int
+num2 = 2.8 #float
+num3 = 1j #complexn
+
+y = type(num1)
+
+print(y)
