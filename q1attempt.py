82f0657f6edde84e0778a2f734a907978e47b99e
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-03 16:30:22 +0100

    Attempts using random

--- q1attempt.py
+++ q1attempt.py
@@ -7,15 +7,25 @@
 #Python has arrays but they have to be imported
 
 import array as arr #We import the array module and can designate it as arr as a shorthand
+import random
 
 A = arr.array('I', [0]) * 256
 B = arr.array('I', [0]) * 300
+x = 0
 
 def pop_arrays():
+    """
     y = type(A)
     print(y)
 
-    A.append(4) #Adds the number 4 to the end of the array
+    A.append(4) #Adds the number 4 to the end of the array (increases array size)
     print(A)
+    """
+
+    for x in A:
+        A[x] = random.randint(0, 1024)
+        test = random.randint(0,1024)
+        x += 1
+        print(A[x])
 
 pop_arrays()
