cabb97397de59e12e86948c6948cc6c822e190ca
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-05 14:22:23 +0100

    Attempt at quicksort. Has index error due to i

--- q1attempt.py
+++ q1attempt.py
@@ -11,12 +11,13 @@ import random
 
 A = arr.array('I', [0]) * 256
 B = arr.array('I', [0]) * 300
+
 x = 0
 #Implement random seed
 
 
 def pop_arrays():
-    
+
 #https://treyhunner.com/2016/04/how-to-loop-with-indexes-in-python/
 
     for x in range(len(A)):
@@ -27,9 +28,34 @@ def pop_arrays():
 
     print("Both arrays have now been populated")
 
-def sort_B():
+def quicksort(array, first, last):
+    """Sorts array using quicksort
+    Arguments:
+        array {int array} -- int array of size >= 256
+        first {index} -- first element
+        last {index} -- last element
+    """
+
+    if first < last:
+        pivot = partition(array, first, last)
+        quicksort(array, first, pivot - 1)
+        quicksort(array, pivot + 1, last)
     #https://www.geeksforgeeks.org/python-program-for-quicksort/
+    #https://github.com/gbrunofranco/PythonAlgorithms/blob/master/sort/quick_sort.py
 
 
+    return array
+
+def partition(array, first, last):
+    pivot = array[last]
+    i = last - 1
+    for j in range(first, last):
+        if array[j] < pivot:
+            i += 1 #Error out of index occurs here
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[last] = array[last], array[i + 1]
+    return i + 1
 
 pop_arrays()
+quicksort(B,0,len(B)-1)
