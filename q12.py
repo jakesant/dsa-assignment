4fe7fa9d65ad2b979e69813bf74b99dc53b41cab
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-13 11:40:45 +0100

    Need to fix summation to start from 1

--- q12.py
+++ q12.py
@@ -10,20 +10,24 @@ def sumFib(nterms):
     x = nterms
     sum = 0
 
-    while nterms != 0:
+    while nterms != 1:
         sum += fib(nterms)
         nterms -= 1
 
     print("The sum of the first", x, "values is", sum)
 
-def fib(n): #General recursive Fibonacci program
+def fib(n): #Recursive Fibonacci program
     if(n<0):
-        print("The number entered cannot be negative")
+        print("The number entered cannot be negtive")
     elif(n==1):
-        return 0
+        return 1
     elif(n==2):
         return 1
     else:
         return fib(n-1) + fib(n-2)
+
+def main():
+    print("This program will calculate the sum of the first X numbers in the Fibonnaci sequence")
+    numInput = input("Enter a number")
 
 sumFib(5)
