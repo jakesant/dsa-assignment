0895814a47f166e7e47d46c9940496cb4253c2a9
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-19 17:07:54 +0100

    Removed print statements in if statement. Return value is given through print statement calling function

--- q6.py
+++ q6.py
@@ -8,22 +8,16 @@
 
 def prime(n):
     if (n % 2 != 0) and (n % 3 != 0) and (n % 5 != 0):
-        print("Das is true")
         return True
     elif n == 1:
-        print("Das is false")
         return False        
     elif n == 2:
-        print("Das is true")
         return True
     elif n == 3:
-        print("Das is true")
         return True
     elif n == 5:
-        print("Das is true")
         return True        
     else:
-        print("Das is false")
         return False
 
-prime(1)
+print(prime(1))
