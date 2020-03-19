1fd6655c0786ba3c78828eb462eb638d72be54c3
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-19 19:30:54 +0100

    Basic function declaration. Base cases set

--- q10.py
+++ q10.py
@@ -1,2 +1,16 @@
 #Write a recursive function that finds the largest number in a given list of integers.
 
+#For this program we could either use a list or a tuple. They are similar but have 1 key differences
+#List - Ordered and we can change values
+#Tuple - Ordered and we cannot change values
+
+
+numlist = [21,420,88,69,666,333,7,47]
+
+def large:
+    if(len(numlist) == 0):
+        print("This is an empty list.")
+    if(len(numlist) == 1):
+        print("The largest number is", numlist[0])
+    else:
+        
