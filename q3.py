b7d5df30232f6248535bfe30cd6bdeef5ee4722b
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-24 08:25:23 +0100

    Must fix if statement for conditions of excluding first and last element

---
+++ q3.py
@@ -0,0 +1,26 @@
+#Let A be an array of n elements (that is, the elements of A are A[0], . . . , A[n−1]).
+#An element A[i] is extreme if the following conditions hold regarding A[i]:
+#   A[i] is not the first nor the last element of A. That is, 0 < i < n – 1
+#   and either A[i − 1] < A[i] > A[i + 1] or A[i − 1] > A[i] < A[i + 1]. For
+#   example, the extreme points of an array [0, 5, 3, 6, 8, 7, 15, 9] are
+#   5, 3, 8, 7, 15. 
+
+#Write an algorithm that prints the extreme points of the given array. If
+#there are no extreme points, the algorithm prints “SORTED”. Do you
+#agree that an array has no extreme points if and only if it is sorted?
+#Explain your answer.
+
+numlist = [1, 2, 3, 4, 5, 6]
+
+#Some pseudocode
+#EXTREME = TRUE if i != 0 and i != n-1
+
+def findExtreme(A):
+    result = [] #list that will hold the extreme points
+    
+    for i in range(len(A) - 1):
+        if (A[i] > A[i-1] and A[i] > A[i+1]) or (A[i] < A[i-1] and A[i] < A[i+1]):
+            result.append(A[i])            
+    print(result)
+
+findExtreme(numlist)
