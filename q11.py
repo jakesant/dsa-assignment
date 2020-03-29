6ca489ef81f0c5fbbfdad0117dee6d2946517e51
Jake Sant <jake.sant.18@um.edu.mt>
2020-03-29 16:59:53 +0200

    Basic function declaration; recursive factorial function implemented

--- q11.py
+++ q11.py
@@ -1,1 +1,20 @@
 #Write a function that computes cosine or sine by taking the first n terms of the appropriate series expansion
+
+#To evaluate the value of sin and cos, we can make use of MacLaurin's Theorem
+#f(x) = f(0) + f'(0)x+ f''(0)/2! x^2 + f'''0/3! x^3 ...
+
+import math
+
+def cos(n):
+    print("nig")
+
+def sin(n):
+    result = 0.0
+
+def fact(n):
+    if (n == 0):
+        return 0
+    else:
+        return n * fact(n-1)
+xyz = 2 ** 10
+print(xyz)
