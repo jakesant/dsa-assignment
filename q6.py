9e444ed01a66924a954a3a29c6b0bea92892c7d0
Jake Sant <jake.sant.18@um.edu.mt>
2020-05-01 10:51:32 +0200

    Removed old Sieve function, added description and successful implementation of Sieve algorithm

--- q6.py
+++ q6.py
@@ -24,14 +24,36 @@ def prime(n):
         return False
 
 
-def sieve(arr):
-    for x in range(len(arr)):
-        if prime(arr[x]) == True:
-            arr[x] = 0
-            
+def sieve(n):
 
-    print(numlist)
+    """Description:
+    Create a list of consecutive numbers starting from 2 to n
+    let prime variable = 2 (first prime number)
+    Starting from p2, count up in increments of p and mark
+    each of these numbers greater than or equal to p2 itself in the list.
+    These numbers will be p(p+1), p(p+2), p(p+3), etc.
+    Find the first number greater than p in the list that is not marked.
+    If there was no such number, stop. Otherwise, let p now equal this number
+    (which is the next prime), and repeat from step 3
+    """
 
+    sievelist = [True for i in range(n+1)] #Creates a list with the values of True going from 0 to n+1
+    primelist = [] #Initiates list which will store values of prime numbers
+    p = 2
+
+    while((p * p) <= n):
+
+        if(sievelist[p] == True): #If number we have set as P is prime
+            for i in range(p*p, n+1, p):
+                sievelist[i] = False
+        p=p+1
+
+    for p in range(2, n):
+        if sievelist[p]:
+            primelist.append(p) #Adds the prime number we have found to the list
+
+    print("The prime numbers smaller than",n,"are:")
+    print(primelist)
 #print(prime(1)) #Test case for checking prime number
 
-sieve(numlist)
+sieve(36)
