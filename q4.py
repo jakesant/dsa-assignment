37989f716dc780d1b4f82396e6d5f93e19d7a219
Jake Sant <jake.sant.18@um.edu.mt>
2020-05-01 10:52:13 +0200

    Initial Commit

---
+++ q4.py
@@ -0,0 +1,6 @@
+#Write a program that, given a list of integers, finds all 2-pairs of integers that have the same product.
+#A 2-pair is 2 distinct pairs of integers ((a,b),(c,d)) where a X b = c X d and a ≠ b ≠ c ≠ d.
+#The range of integers in the list should be from 1 to 1024.
+
+def pairs():
+    
