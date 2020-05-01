16b1b15e30717aaf3458dbf5d91dbbd08a19da62
Jake Sant <jake.sant.18@um.edu.mt>
2020-05-01 12:51:42 +0200

    Testing stack operations

--- q5.py
+++ q5.py
@@ -7,10 +7,33 @@ import operator
 ops = { '+': operator.add,
         '-': operator.sub,
         'x': operator.mul,
-        '/': operator.div
+        '/': operator.truediv
 }
 
-def stack():
+#Operators will apply on 2 operands
 
+def evaluate(expression):
+    stack = []
 
-expression = ["2", "1", "+", "3", "*"]
+    for char in expression:
+        if char.isdigit():
+            stack.append(char)
+            print("pushing", char)
+        if not char:
+            break
+
+    print("The contents of the stack is: ", stack)
+    stack.pop()
+    print("The contents of the stack is: ", stack)
+
+rpnexp = ["2", "1", "+", "3", "*"]
+evaluate(rpnexp)
+
+"""Psuedocode:
+    Read through expression in a list character by character
+    If char is number, push onto the stack
+    if we meet an operand, we
+        pop last two numbers from stack
+        work out that operation
+        push result into stack
+"""
