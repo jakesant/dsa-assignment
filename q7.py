5f0535081e51ecf3f9506eea0b4dfd7a1e231353
Jake Sant <jake.sant.18@um.edu.mt>
2020-05-01 13:19:08 +0200

    Added question

---
+++ q7.py
@@ -0,0 +1,3 @@
+#Write a program that accepts as input a sequence of integers (one by
+#one) and then incrementally builds, and displays, a Binary Search Tree
+#(BST). There is no need to balance the BST.
